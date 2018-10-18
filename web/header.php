645446dc18fca94860c4dae73118789ee96b09b5
Russ Flynn <russ@custard.no>
2018-10-19 00:56:11 +0200

    Fix header... (bad merge)

--- web/header.php
+++ web/header.php
@@ -3,10 +3,6 @@
   <head>
     <title>ENRK Destinations</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<<<<<<< HEAD
-    <link rel="stylesheet" type="text/css" media="screen" href="/styles.css" />
-=======
     <link rel="stylesheet" type="text/css" media="screen" href="styles.css" />
->>>>>>> 60278aadd6453a595aee828b0a0388adfda54e57
   </head>
   <body>
